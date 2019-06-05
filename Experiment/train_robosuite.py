12a2d64cc2ce14cb8eba0535af03b357f97e578d
cheneyuwu <cheneyuwu@gmail.com>
2019-06-05 15:38:56 -0400

    a much simper render script

--- Experiment/train_robosuite.py
+++ Experiment/train_robosuite.py
@@ -1,57 +1,82 @@
 import os
-from train import Demo, Train, Display, Plot
+from collections import OrderedDict
+import numpy as np
+from yw.util.cmd_util import Command
+from train import Experiment, Demo, Train, Display, Plot
 
-demo_exp = Demo()
-train_exp = Train()
-display_exp = Display()
-plot_exp = Plot()
 
-environment = "SawyerLift"
-train_exp.env = environment
-train_exp.num_cpu = 3
-train_exp.update()
+class Render(Experiment):
+    """For rendering the environment
+    """
 
-train_rl_epochs = 50
-seed = 1
+    def __init__(self):
+        super().__init__()
+        self.run = OrderedDict([("python", os.path.join(self.flow_dir, "render/robosuite_render.py"))])
 
-# for i in range(3):
-#     seed += i * 100
+    @Command.execute
+    def render(self, **override):
+        command = self.run.copy()
+        command["--policy_file"] = self.result_dir + "RLDemoCriticPolicy/rl/policy_latest.pkl"
+        command["--env_arg"] = {
+            "has_renderer": "bool:*",
+            "use_object_obs": "bool:*",
+            "use_camera_obs": "bool:",
+            "ignore_done": "bool:*",
+            "control_freq": "int:50",
+        }
+        command["--num_itr"] = 10
+        command["--eps_length"] = 1000
+        return command
 
-#     # We can change the result directory without updating
-#     exp_dir = os.getenv("EXPERIMENT")
-#     result_dir = os.path.join(exp_dir, "Result/Temp/Exp" + str(i) + "/")
-#     demo_exp.result_dir = result_dir
-#     train_exp.result_dir = result_dir
 
-#     assert not train_exp.rl_only(
-#         rl_scope="rl_only",
-#         n_cycles=20,
-#         seed=seed + 10,
-#         rl_num_sample=1,
-#         rl_batch_size=256,
-#         train_rl_epochs=train_rl_epochs,
-#         rollout_batch_size=4,
-#         n_test_rollouts=5,  # for evaluation only, cannot make this larger because we will run out of memory
-#         env_arg={
-#             "has_renderer": False,  # no on-screen renderer
-#             "has_offscreen_renderer": False,  # no off-screen renderer
-#             "use_object_obs": True,  # use object-centric feature
-#             "use_camera_obs": False,  # no camera observations)
-#             "reward_shaping": True,  # use dense rewards
-#         },
-#     )
+if __name__ == "__main__":
 
-# # Plot the training result
-# plot_exp.plot(dirs=plot_exp.result_dir)
+    demo_exp = Demo()
+    train_exp = Train()
+    display_exp = Display()
+    plot_exp = Plot()
+    render_exp = Render()
 
-# Display a policy result (calls run_agent).
-assert not display_exp.display(
-    policy_file=display_exp.result_dir + "../Jun05RoboSuite/Exp0/RLNoDemo/rl/policy_latest.pkl",
-    env_arg={
-        "has_renderer": True,  # no on-screen renderer
-        # "has_offscreen_renderer": True,  # no off-screen renderer
-        # "use_object_obs": True,  # use object-centric feature
-        # "use_camera_obs": False,  # no camera observations)
-        # "reward_shaping": True,  # use dense rewards
-    },
-)
+    environment = "SawyerLift"
+    train_exp.env = environment
+    train_exp.num_cpu = 3
+    train_exp.update()
+
+    train_rl_epochs = 50
+    seed = 1
+
+    # for i in range(3):
+    #     seed += i * 100
+
+    #     # We can change the result directory without updating
+    #     exp_dir = os.getenv("EXPERIMENT")
+    #     result_dir = os.path.join(exp_dir, "Result/Temp/Exp" + str(i) + "/")
+    #     demo_exp.result_dir = result_dir
+    #     train_exp.result_dir = result_dir
+
+    #     assert not train_exp.rl_only(
+    #         rl_scope="rl_only",
+    #         n_cycles=20,
+    #         seed=seed + 10,
+    #         rl_num_sample=1,
+    #         rl_batch_size=256,
+    #         train_rl_epochs=train_rl_epochs,
+    #         rollout_batch_size=4,
+    #         n_test_rollouts=5,  # for evaluation only, cannot make this larger because we will run out of memory
+    #         env_arg={
+    #             "has_renderer": False,  # no on-screen renderer
+    #             "has_offscreen_renderer": False,  # no off-screen renderer
+    #             "use_object_obs": True,  # use object-centric feature
+    #             "use_camera_obs": False,  # no camera observations)
+    #             "reward_shaping": True,  # use dense rewards
+    #             "control_freq": 50,
+    #         },
+    #     )
+
+    # Plot the training result
+    # plot_exp.plot(dirs=plot_exp.result_dir)
+
+    # Render a policy result (calls robosuite_render.py).
+    assert not render_exp.render(
+        policy_file=render_exp.result_dir + "../Jun05RoboSuite/Exp0/RLNoDemo/rl/policy_latest.pkl"
+    )
