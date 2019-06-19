a74978d46156b498414048d4a4c5b12d7d99e28e
cheneyuwu <cheneyuwu@gmail.com>
2019-06-19 12:08:35 -0400

    state action dependent potential

--- Package/yw/yw/ddpg_main/demo_shaping.py
+++ Package/yw/yw/ddpg_main/demo_shaping.py
@@ -1,26 +1,52 @@
-import numpy as np
+import tensorflow as tf
+
+# class DemoShaping:
+#     def __init__(self, gamma):
+#         self.gamma = gamma
+    
+#     def get_reward(self, o, g, u, o_2, g_2, u_2, **kwargs):
+#         """
+#         Compute the reward based on discounted difference of potentials.
+#         """
+#         return self.gamma * self.get_potential(o_2, g_2, u_2) - self.get_potential(o, g, u)
+    
+#     def get_potential(self, o, g, u):
+#         pass
+
+
+
+# class ReacherStateDemoShaping(DemoShaping):
+#     def __init__(self, gamma):
+#         super().__init__(gamma)
+    
+#     def get_potential(self, o, g, u):
+#         """
+#         Just return negative value of distance between current state and goal state
+#         """
+#         return -np.abs(o-g)
+
 
 class DemoShaping:
-    def __init__(self, gamma):
-        self.gamma = gamma
-    
-    def get_reward(self, o, g, u, o_2, g_2, u_2, **kwargs):
+    def __init__(self, o, g, u, o_2, g_2, u_2, num_sample, gamma):
         """
-        Compute the reward based on discounted difference of potentials.
+        Implement the state, action based potential function and corresponding actions
+        Args:
+            o - observation
+            g - goal
+            u - action
+            o_2 - observation that goes to
+            g_2 - same as g, idk why I must make this specific
+            u_2 - output from the actor of the main network
         """
-        return self.gamma * self.get_potential(o_2, g_2, u_2) - self.get_potential(o, g, u)
+        self.potential = self.calc_potential(o, g, u)
+        self.reward = []
+        for i in range(num_sample):
+            next_potential = self.calc_potential(o_2, g_2, u_2[i])
+            self.reward.append(10 * (gamma * next_potential - self.potential))
     
-    def get_potential(self, o, g, u):
-        pass
-
-
-
-class ReacherStateDemoShaping(DemoShaping):
-    def __init__(self, gamma):
-        super().__init__(gamma)
-    
-    def get_potential(self, o, g, u):
+    def calc_potential(self, o, g, u):
         """
         Just return negative value of distance between current state and goal state
         """
-        return -np.abs(o-g)
+        ret = -tf.abs(o-g+0.5)
+        return ret
