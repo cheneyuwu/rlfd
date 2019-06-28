aad1efb85da524ec7b6d8ee7585d483584639113
cheneyuwu <cheneyuwu@gmail.com>
2019-06-27 21:08:45 -0400

    generate demo

--- Experiment/Environment/fetch_data_generation.py
+++ Experiment/Environment/fetch_data_generation.py
@@ -10,8 +10,7 @@ infos = []
 
 def main():
     env = gym.make('FetchPickAndPlace-v1')
-    numItr = 100
-    initStateSpace = "random"
+    numItr = 2
     env.reset()
     print("Reset!")
     while len(actions) < numItr:
@@ -19,17 +18,62 @@ def main():
         print("ITERATION NUMBER ", len(actions))
         goToGoal(env, obs)
 
+    T = env._max_episode_steps
+
+    # info_keys = [key.replace('info_', '') for key in self.input_dims.keys() if key.startswith('info_')]
+    # info_values = [np.empty((self.T - 1, 1, self.input_dims['info_' + key]), np.float32) for key in info_keys]
+
+    demo_data_obs = observations
+    demo_data_acs = actions
+    demo_data_info = infos
+    print(infos)
+    exit(1)
+
+    result = None
+
+    for epsd in range(numItr): # we initialize the whole demo buffer at the start of the training
+        obs, acts, goals, achieved_goals = [], [] ,[] ,[]
+        i = 0
+        for transition in range(T):
+            obs.append([demo_data_obs[epsd][transition].get('observation')])
+            acts.append([demo_data_acs[epsd][transition]])
+            goals.append([demo_data_obs[epsd][transition].get('desired_goal')])
+            achieved_goals.append([demo_data_obs[epsd][transition].get('achieved_goal')])
+            # for idx, key in enumerate(info_keys):
+            #     info_values[idx][transition, i] = demo_data_info[epsd][transition][key]
+
+
+        obs.append([demo_data_obs[epsd][T].get('observation')])
+        achieved_goals.append([demo_data_obs[epsd][T].get('achieved_goal')])
+
+        episode = dict(o=obs,
+                        u=acts,
+                        g=goals,
+                        ag=achieved_goals)
+        # for key, value in zip(info_keys, info_values):
+        #     episode['info_{}'.format(key)] = value
+
+        # switch to batch major
+        episode_batch = {}
+        for key in episode.keys():
+            val = np.array(episode[key]).copy()
+            # make inputs batch-major instead of time-major
+            episode_batch[key] = val.swapaxes(0, 1)
+        episode = episode_batch
+
+        if result == None:
+            result = episode
+        else:
+            for k in result.keys():
+                result[k] = np.concatenate((result[k], episode[k]), axis=0)
+                print(result[k].shape)
+
 
     fileName = "data_fetch"
-    fileName += "_" + initStateSpace
     fileName += "_" + str(numItr)
     fileName += ".npz"
 
-    print(actions)
-    print(observations)
-    print(infos)
-
-    np.savez_compressed(fileName, acs=actions, obs=observations, info=infos) # save the file
+    np.savez_compressed(fileName, **result) # save the file
 
 def goToGoal(env, lastObs):
 
