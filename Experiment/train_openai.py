7b92c1366b008c81d119cf0ccc850e45ba57b581
cheneyuwu <cheneyuwu@gmail.com>
2019-06-30 21:01:36 -0400

    update training script

--- Experiment/train_openai.py
+++ Experiment/train_openai.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # If you want to use dense reward, add "Dense" to the reward name and make sure the env manager recognizes that.
     # Please follow this convention so that you can plot the same env with different reward types in the same graph.
     environment = "FetchPickAndPlaceDense-v1"
-    demo_data_size = 64
+    demo_data_size = 48
     seed = 0 # change seed value inside the for loop
 
     train_exp.set_shared_cmd(
@@ -30,25 +30,25 @@ if __name__ == "__main__":
         n_cycles=10,
         rl_num_sample=1,
         rl_batch_size=256,
-        train_rl_epochs=512,
+        train_rl_epochs=128,
     )
 
     demo_exp.set_shared_cmd(
         num_demo=demo_data_size,
     )
 
-    for i in range(1):
+    for i in range(3):
 
         # Change seed value in each iteration
         seed += i * 100
 
         # Change the result directory so that different seed goes to different directory
-        train_exp.result_dir = result_dir
-        demo_exp.result_dir = result_dir
+        train_exp.result_dir = os.path.join(result_dir, "Exp"+ str(i))
+        demo_exp.result_dir = os.path.join(result_dir, "Exp"+ str(i))
 
         # Train the RL without demonstration using dense reward.
         # train_exp.rl_only_dense(
-        #     env="FetchPickAndPlaceDense-v1", # use env with dense reward
+        #     env="FetchReachDense-v1", # use env with dense reward
         #     r_shift=0.0,
         #     seed=seed + 0,
         # )
@@ -69,17 +69,17 @@ if __name__ == "__main__":
 
         # Train the RL with demonstration through BC
         train_exp.rl_with_bc(
-            seed=seed + 40,
+            seed=seed + 30,
             rl_batch_size_demo=128,
             num_demo=demo_data_size,
-            demo_file=os.path.join(train_exp.result_dir, "DemoData", environment+".npz"),
+            demo_file=os.path.join(train_exp.result_dir, "../DemoData", environment+".npz"),
         )
 
         # Train the RL with demonstration through shaping
         train_exp.rl_with_shaping(
-            seed=seed + 30,
+            seed=seed + 40,
             num_demo=demo_data_size,
-            demo_file=os.path.join(train_exp.result_dir, "DemoData", environment+".npz"),
+            demo_file=os.path.join(train_exp.result_dir, "../DemoData", environment+".npz"),
             # shaping_policy=os.path.join(train_exp.result_dir, "RLDemoShaping", "shaping/shaping_latest.ckpt"),
         )
 
