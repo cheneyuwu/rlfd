27257180a1454f14e73c916e1f2d0fbd8c0aeffd
cheneyuwu <cheneyuwu@gmail.com>
2019-07-08 09:50:12 -0400

    cosmetic changes

--- Package/yw/yw/ddpg_main/config.py
+++ Package/yw/yw/ddpg_main/config.py
@@ -13,18 +13,18 @@ from yw.env.env_manager import EnvManager
 
 
 DEFAULT_PARAMS = {
-    # config name
+    # Config Summary
     "config": "default",
-    # env
+    # Environment Config
     "env_name": "FetchPickAndPlace-v1",
     "r_scale": 1.0,  # re-scale the reward. Only use this for dense rewards.
     "r_shift": 0.0,  # re-scale the reward. Only use this for dense rewards.
     "eps_length": 0,  # change the length of the episode.
     "env_args": {},  # extra arguments passed to the environment.
     "max_u": 1.0,  # max absolute value of actions on different coordinates
-    # general ddpg config
-    "rl_buffer_size": int(1e6),  # for experience replay
-    "rl_scope": "ddpg",  # can be tweaked for testing
+    # DDPG Config
+    "rl_buffer_size": int(1e6),
+    "rl_scope": "ddpg",  
     "rl_use_td3": 1,  # whether or not to use td3
     "rl_num_sample": 1,  # number of ensemble of actor_critics
     "rl_layers": 3,  # number of layers in the critic/actor networks
@@ -34,37 +34,37 @@ DEFAULT_PARAMS = {
     "rl_action_l2": 1.0,  # quadratic penalty on actions (before rescaling by max_u)
     "rl_batch_size": 256,  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
     "rl_batch_size_demo": 128,  # number of samples to be used from the demonstrations buffer, per mpi thread 128/1024 or 32/256
-    "rl_demo_critic": "none",  # whether or not to use the behavior cloning loss as an auxilliary loss
-    "rl_demo_actor": "none",  # whether or not to use the behavior cloning loss as an auxilliary loss
-    "rl_q_filter": 0,  # whether or not a Q value filter should be used on the Actor outputs
+    "rl_demo_critic": "none",  # whether or not to use shaping
+    "rl_demo_actor": "none",  # whether or not to use bc
+    "rl_q_filter": 1,  # whether or not a Q value filter should be used on the actor outputs
     "rl_num_demo": 1000,  # number of expert demo episodes
-    "rl_prm_loss_weight": 1.0,  # Weight corresponding to the primary loss
-    "rl_aux_loss_weight": 1.0,  # Weight corresponding to the auxilliary loss also called the cloning loss
-    # Double Q Learning
-    "rl_polyak": 0.95,  # polyak averaging coefficient
-    # Normalization - not used for now due to usage of demonstration
+    "rl_prm_loss_weight": 0.001,  # weight corresponding to the primary loss
+    "rl_aux_loss_weight": 0.0078,  # weight corresponding to the auxilliary loss also called the cloning loss
+    # double q learning
+    "rl_polyak": 0.95,  # polyak averaging coefficient for double q learning
+    # normalization
     "rl_norm_eps": 0.01,  # epsilon used for observation normalization
     "rl_norm_clip": 5,  # normalized observations are cropped to this values
-    # Process I/Os
+    # i/o clippings
     "rl_clip_obs": 200.0,
     "rl_clip_pos_returns": False,  # Whether or not this environment has positive return or not.
     "clip_return": False,
-    # Exploration - for rollouts
+    # replay strategy to be used
+    "rl_replay_strategy": "none",  # supported modes: future, none for uniform
+    "rl_demo_replay_strategy": "none",  # supported modes: future, none for uniform
+    # HER Config
+    "her_k": 4,  # number of additional goals used for replay, only used if off_policy_data=future
+    # Rollouts Config
     "explore": 1,  # whether or not to use e-greedy and add noise to output
     "random_eps": 0.3,  # percentage of time a random action is taken
     "noise_eps": 0.2,  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
-    # Replay strategy to be used
-    "rl_replay_strategy": "none",  # supported modes: future, none for uniform
-    "rl_demo_replay_strategy": "none",  # supported modes: future, none for uniform
-    # HER
-    "her_k": 4,  # number of additional goals used for replay, only used if off_policy_data=future
-    # DDPG Training
+    # Training Config
     "train_rl_epochs": 1,
     "n_cycles": 10,  # per epoch
     "n_batches": 40,  # training batches per cycle
     "rollout_batch_size": 4,  # per mpi thread
     "n_test_rollouts": 20,  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
-    # Testing
+    # Testing Config
     "test_with_polyak": False,  # run test episodes with the target network
 }
 
