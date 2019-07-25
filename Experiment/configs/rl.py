cdc033ba9605fcaf111bf9562052263c1de20329
Yuchen Wu <yuchenwu@gra-login3.graham.sharcnet>
2019-07-25 10:32:58 -0400

    update params and default environments

--- Experiment/configs/rl.py
+++ Experiment/configs/rl.py
@@ -2,7 +2,7 @@ params_config = {
     # Config Summary
     "config": ("RL",),  # change this for each customized params file
     # Environment Config
-    "env_name": "FetchReach-v1",
+    "env_name": "FetchPickAndPlace-v1",
     "r_scale": 1.0,  # scale the reward of the environment down
     "r_shift": 0.0,  # shift the reward of the environment up
     "eps_length": 0,  # overwrite the default length of the episode
@@ -29,7 +29,7 @@ params_config = {
         "q_filter": 1,  # whether or not a Q value filter should be used on the actor outputs
         "prm_loss_weight": 0.001,  # weight corresponding to the primary loss
         "aux_loss_weight": 0.0078,  # weight corresponding to the auxilliary loss also called the cloning loss
-        "shaping_params": {"prm_loss_weight": 1.0, "reg_loss_weight": 500.0, "potential_weight": 5.0},
+        "shaping_params": {"prm_loss_weight": 1.0, "reg_loss_weight": 800.0, "potential_weight": 4.0},
         # normalization
         "norm_eps": 0.01,  # epsilon used for observation normalization
         "norm_clip": 5,  # normalized observations are cropped to this values
--- Experiment/configs/rlmerged.py
+++ Experiment/configs/rlmerged.py
@@ -4,4 +4,3 @@ params_config = base_params.copy()
 params_config["config"] = "default"
 params_config["seed"] = tuple(range(4, 8))
 params_config["ddpg"]["demo_strategy"] = ("none", "bc", "maf", "rb", "rbmaf")
-params_config["ddpg"]["shaping_params"]["potential_weight"] = 4.0
