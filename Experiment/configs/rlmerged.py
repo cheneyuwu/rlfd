dc98683a75eae14ca2623c35c80967734c699a76
Yuchen Wu <cheneyuwu@gmail.com>
2019-07-25 16:26:02 -0400

    10 seeds

--- Experiment/configs/rlmerged.py
+++ Experiment/configs/rlmerged.py
@@ -2,5 +2,5 @@ from .rl import params_config as base_params
 
 params_config = base_params.copy()
 params_config["config"] = "default"
-params_config["seed"] = tuple(range(4, 8))
+params_config["seed"] = tuple(range(10))
 params_config["ddpg"]["demo_strategy"] = ("none", "bc", "maf", "rb", "rbmaf")
