0eae50ae12eb42e1a85b1e266f0b6beb0410d94b
Florian Shkurti <florian.shkurti@gmail.com>
2019-08-18 23:21:46 -0400

    Added panda client

--- Panda/experts/pick_and_place.py
+++ Panda/experts/pick_and_place.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-## BEGIN_SUB_TUTORIAL imports
-##
-## To use the Python MoveIt! interfaces, we will import the `moveit_commander`_ namespace.
-## This namespace provides us with a `MoveGroupCommander`_ class, a `PlanningSceneInterface`_ class,
-## and a `RobotCommander`_ class. (More on these below)
-##
-## We also import `rospy`_ and some messages that we will use:
-##
-
 import sys
 import copy
 import rospy
@@ -18,158 +9,70 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
-
-home_pose = geometry_msgs.msg.Pose()
-home_pose.orientation.x = 1.0
-home_pose.orientation.y = 0.0
-home_pose.orientation.z = 0.0
-home_pose.orientation.w = 0.0
-home_pose.position.x = 0.56
-home_pose.position.y = 0.0
-home_pose.position.z = 0.37
-    
-
-def all_close(goal, actual, tolerance):
-  """
-  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-  @param: goal       A list of floats, a Pose or a PoseStamped
-  @param: actual     A list of floats, a Pose or a PoseStamped
-  @param: tolerance  A float
-  @returns: bool
-  """
-  all_equal = True
-  if type(goal) is list:
-    for index in range(len(goal)):
-      if abs(actual[index] - goal[index]) > tolerance:
-        return False
-
-  elif type(goal) is geometry_msgs.msg.PoseStamped:
-    return all_close(goal.pose, actual.pose, tolerance)
-
-  elif type(goal) is geometry_msgs.msg.Pose:
-    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
-
-  return True
-
-class MoveGroupPythonIntefaceTutorial(object):
-  """MoveGroupPythonIntefaceTutorial"""
-  def __init__(self):
-    super(MoveGroupPythonIntefaceTutorial, self).__init__()
-
-    ## BEGIN_SUB_TUTORIAL setup
-    ##
-    ## First initialize `moveit_commander`_ and a `rospy`_ node:
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
-
-    ## Instantiate a `RobotCommander`_ object. This object is the outer-level interface to
-    ## the robot:
-    robot = moveit_commander.RobotCommander()
-
-    ## Instantiate a `PlanningSceneInterface`_ object.  This object is an interface
-    ## to the world surrounding the robot:
-    scene = moveit_commander.PlanningSceneInterface()
-
-    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-    ## to one group of joints.  In this case the group is the joints in the Panda
-    ## arm so we set ``group_name = panda_arm``. If you are using a different robot,
-    ## you should change this value to the name of your robot arm planning group.
-    ## This interface can be used to plan and execute motions on the Panda:
-    group_name = "panda_arm"
-    group = moveit_commander.MoveGroupCommander(group_name)
-
-    # Slow down the max velocity of the robot, just like in rviz
-    group.set_max_velocity_scaling_factor(0.2)
-    group.set_max_acceleration_scaling_factor(0.2)
-    
-    
-    ## We create a `DisplayTrajectory`_ publisher which is used later to publish
-    ## trajectories for RViz to visualize:
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                   moveit_msgs.msg.DisplayTrajectory,
-                                                   queue_size=20)
-
-    ## END_SUB_TUTORIAL
-
-    ## BEGIN_SUB_TUTORIAL basic_info
-    ##
-    ## Getting Basic Information
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-    # We can get the name of the reference frame for this robot:
-    planning_frame = group.get_planning_frame()
-    print "============ Reference frame: %s" % planning_frame
-
-    # We can also print the name of the end-effector link for this group:
-    eef_link = group.get_end_effector_link()
-    print "============ End effector: %s" % eef_link
-
-    # We can get a list of all the groups in the robot:
-    group_names = robot.get_group_names()
-    print "============ Robot Groups:", robot.get_group_names()
-
-    # Sometimes for debugging it is useful to print the entire state of the
-    # robot:
-    print "============ Printing robot state"
-    print robot.get_current_state()
-    print ""
-    ## END_SUB_TUTORIAL
-
-    # Misc variables
-    self.box_name = ''
-    self.robot = robot
-    self.scene = scene
-    self.group = group
-    self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
-
-  def go_to_pose_goal(self):
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    group = self.group
-
-    ## BEGIN_SUB_TUTORIAL plan_to_pose
-    ##
-    ## Planning to a Pose Goal
-    ## ^^^^^^^^^^^^^^^^^^^^^^^
-    ## We can plan a motion for this group to a desired pose for the
-    ## end-effector:
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.4
-    pose_goal.position.y = 0.1
-    pose_goal.position.z = 0.4
-    group.set_pose_target(pose_goal)
-
-    ## Now, we call the planner to compute the plan and execute it.
-    plan = group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    group.clear_pose_targets()
-
-    ## END_SUB_TUTORIAL
-
-    # For testing:
-    # Note that since this section of code will not be included in the tutorials
-    # we use the class variable rather than the copied state variable
-    current_pose = self.group.get_current_pose().pose
-    return all_close(pose_goal, current_pose, 0.01)
+import actionlib
+import franka_gripper
+import franka_gripper.msg
+from panda_client import PandaClient 
 
 
 def main():
   try:
-    print "============ Press `Enter` to begin the tutorial by setting up the moveit_commander (press ctrl-d to exit) ..."
-    raw_input()
-    tutorial = MoveGroupPythonIntefaceTutorial()
-    
-    print "============ Press `Enter` to execute a movement using a pose goal ..."
-    raw_input()
-    tutorial.go_to_pose_goal()
 
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('panda_experiment', anonymous=True)
+
+    # This is an interface to the world surrounding the robot
+    scene = moveit_commander.PlanningSceneInterface()
+
+    # This is an interface to the robot
+    panda_client = PandaClient()
+    
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.x = 1.0
+    pose_goal.orientation.y = 0.0
+    pose_goal.orientation.z = 0.0
+    pose_goal.orientation.w = 0.0
+    pose_goal.position.x = 0.36
+    pose_goal.position.y = 0.0
+    pose_goal.position.z = 0.37
+    
+    panda_client.get_ee_state()
+    
+    print "============ Press `1` to move the arm to a fixed valid position..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.move_ee_to(pose_goal)
+
+    print "============ Press `1` to shift the EE by a delta..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.shift_ee_by(axis=0, value=-0.05)
+      
+
+    print "============ Press `1` to move the arm to home configuration..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.move_joints_to(panda_client.home_pose_joint_values)
+      
+      
+    print "============ Press `1` to move the gripper ..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.actionlib_move_gripper_to(width=0.06)
+      
+      
+    print "============ Press `1` to grasp ..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.actionlib_gripper_grasp(width=0.3)
+      
+      
+    print "============ Press `1` to put the gripper in the home position..."
+    inp = raw_input()
+    if inp == '1':
+      panda_client.actionlib_gripper_homing()
+      
+    
     print "============ Python tutorial demo complete!"
   except rospy.ROSInterruptException:
     return
