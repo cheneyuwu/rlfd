58df77abbc04d42d209684568d003e4ee67e2799
Melissa Mozifian <melissafm24@gmail.com>
2019-08-22 16:35:38 -0400

    update panda env

--- Panda/experts/panda_env.py
+++ Panda/experts/panda_env.py
@@ -2,6 +2,7 @@
 
 import sys
 import rospy
+import roslaunch
 import moveit_commander
 import panda_client as panda
 
@@ -9,22 +10,36 @@ import panda_client as panda
 class FrankaPandaRobot:
 
     def __init__(self):
+		pass
+
+    def step(self, action):
+        self.panda.apply_velocity_action(action)
+
+    def reset(self):
+
         try:
+			uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/florian/code/RLProject/Panda/panda_real/launch/panda_moveit.launch"])
+            launch.start()
+
             moveit_commander.roscpp_initialize(sys.argv)
             rospy.init_node('panda_experiment', anonymous=True)
 
-            self.scene = moveit_commander.PlanningSceneInterface()
+            scene = moveit_commander.PlanningSceneInterface()
 
-            self.panda = panda.PandaClient()
+            panda = panda.PandaClient()
+            panda.go_home()
+	    	#rospy.sleep(3)
+	    	#launch.shutdown()
 
         except rospy.ROSInterruptException:
             return
 
-    def step(self, action):
-        self.panda.apply_action(action)
 
-    def reset(self):
-        self.panda.go_home()
+
+    def enable_vel_control(self):
+		pass
 
     def enable_grip(self):
         pass
@@ -36,3 +51,7 @@ class FrankaPandaRobot:
 if __name__ == '__main__':
     panda_robo = FrankaPandaRobot()
     panda_robo.reset()
+
+    #while not rospy.is_shutdown():
+    #	panda_robo.step([0.0, 0.0, 0.0])
+    #    rospy.sleep(0.1)
