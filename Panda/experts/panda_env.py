d0df86966013f13f1e2f4b4116ae5ad549a603ec
Melissa Mozifian <melissafm24@gmail.com>
2019-08-22 17:04:43 -0400

    enable automatic roslaunch spawning

--- Panda/experts/panda_env.py
+++ Panda/experts/panda_env.py
@@ -5,13 +5,16 @@ import rospy
 import roslaunch
 import moveit_commander
 import panda_client as panda
-
+from geometry_msgs.msg import Twist
 
 class FrankaPandaRobot:
 
+    def __init__(self):
+        self.velocity_publisher = rospy.Publisher(
+            "/franka_control/target_velocity", Twist, queue_size=1)
 
     def step(self, action):
-        self.panda.apply_velocity_action(action)
+        self.apply_velocity_action(action)
 
     def reset(self):
 
@@ -28,8 +31,10 @@ class FrankaPandaRobot:
 
             panda_robot = panda.PandaClient()
             panda_robot.go_home()
-            # rospy.sleep(3)
-            # launch.shutdown()
+			# May or may not need to sleep - really depends how long it takes
+			# to finish execution before shutting down panda moveit!
+            #rospy.sleep(3)
+            launch.shutdown()
 
         except rospy.ROSInterruptException:
             return
@@ -37,6 +42,22 @@ class FrankaPandaRobot:
 
 
     def enable_vel_control(self):
+        try:
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid,["/home/florian/code/RLProject/Panda/panda_real/launch/franka_arm_vel_controller.launch"])
+            launch.start()
+
+			# May or may not need to sleep - really depends how long it takes
+			# to finish execution before shutting down panda moveit!
+            #rospy.sleep(3)
+            #launch.shutdown()
+
+        except rospy.ROSInterruptException:
+            return
+
+
+    def disable_vel_control(self):
         pass
 
     def enable_grip(self):
@@ -45,11 +66,21 @@ class FrankaPandaRobot:
     def disable_grip(self):
         pass
 
+    def apply_velocity_action(self, action):
+
+        twist = Twist()
+        twist.linear.x = action[0]
+        twist.linear.y = action[1]
+        twist.linear.z = action[2]
+        self.velocity_publisher.publish(twist)
+
 
 if __name__ == '__main__':
+    rospy.init_node("panda_arm_env")
     panda_robo = FrankaPandaRobot()
-    panda_robo.reset()
+    #panda_robo.reset()
+    panda_robo.enable_vel_control()
 
-    # while not rospy.is_shutdown():
-    #	panda_robo.step([0.0, 0.0, 0.0])
-    #    rospy.sleep(0.1)
+    while not rospy.is_shutdown():
+    	panda_robo.step([0.5, 0.0, 0.0])
+        rospy.sleep(0.1)
