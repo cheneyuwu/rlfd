4b11d47a3c629b4adc61170927394344d3c61aa9
Yuchen Wu <cheneyuwu@gmail.com>
2019-09-12 11:01:24 -0400

    transfer name

--- Package/yw/yw/flow/launch.py
+++ Package/yw/yw/flow/launch.py
@@ -69,46 +69,51 @@ def generate_params(root_dir, param_config):
     return res
 
 
-def transform_config_name(config_name):
-    """ Transfer the legend names"""
-    print(config_name)
-    for i in range(len(config_name)):
-        if config_name[i].startswith("demo_strategy"):
-            if config_name[i] == "demo_strategy_nf":
-                config_name[i] = "MAF Shaping"
-            elif config_name[i] == "demo_strategy_gan":
-                config_name[i] = "GAN Shaping"
-            elif config_name[i] == "demo_strategy_pure_bc":
-                config_name[i] = "BC"
-            elif config_name[i] == "demo_strategy_bc":
-                if "q_filter_1" in config_name:
-                    config_name[i] = "TD3+BC+Q Filter"
-                else:
-                    config_name[i] = "TD3+BC"
-            elif config_name[i] == "demo_strategy_none":
-                config_name[i] = "TD3"
-    return config_name
-
-
 # def transform_config_name(config_name):
 #     """ Transfer the legend names"""
 #     print(config_name)
 #     for i in range(len(config_name)):
 #         if config_name[i].startswith("demo_strategy"):
 #             if config_name[i] == "demo_strategy_nf":
-#                 return ["MAF Shaping"]
+#                 config_name[i] = "MAF Shaping"
 #             elif config_name[i] == "demo_strategy_gan":
-#                 return ["GAN Shaping"]
+#                 config_name[i] = "GAN Shaping"
 #             elif config_name[i] == "demo_strategy_pure_bc":
-#                 return ["BC"]
+#                 config_name[i] = "BC"
 #             elif config_name[i] == "demo_strategy_bc":
 #                 if "q_filter_1" in config_name:
-#                     return ["TD3+BC+Q Filter"]
+#                     config_name[i] = "TD3+BC+Q Filter"
 #                 else:
-#                     return ["TD3+BC"]
+#                     config_name[i] = "TD3+BC"
 #             elif config_name[i] == "demo_strategy_none":
-#                 return ["TD3"]
+#                 config_name[i] = "TD3"
 #     return config_name
+
+def transform_config_name(config_name):
+    """ Transfer the legend names"""
+    print(config_name)
+    for i in range(len(config_name)):
+        if config_name[i].startswith("demo_strategy"):
+            if config_name[i] == "demo_strategy_nf":
+                return ["NF Shaping"]
+            elif config_name[i] == "demo_strategy_gan":
+                return ["GAN Shaping"]
+            elif config_name[i] == "demo_strategy_pure_bc":
+                return ["BC"]
+            elif config_name[i] == "demo_strategy_bc":
+                if "prm_loss_weight_0.001" in config_name:
+                    return ["$\lambda$TD3+BC, $\lambda$=0.001"]
+                elif "prm_loss_weight_0.0001" in config_name:
+                    return ["$\lambda$TD3+BC, $\lambda$=0.0001"]
+                elif "prm_loss_weight_0.01" in config_name:
+                    return ["$\lambda$TD3+BC, $\lambda$=0.01"]
+                elif "prm_loss_weight_0.1" in config_name:
+                    return ["$\lambda$TD3+BC, $\lambda$=0.1"]
+                else:
+                    return ["TD3+BC"]
+            elif config_name[i] == "demo_strategy_none":
+                return ["TD3"]
+    return config_name
 
 
 def main(targets, exp_dir, policy_file, **kwargs):
