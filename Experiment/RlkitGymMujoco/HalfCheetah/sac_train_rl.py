d5d29f13a8f3762dc2dba1b9d03e28903a69e73a
cheneyuwu <cheneyuwu@gmail.com>
2020-01-15 23:54:33 -0500

    halfcheetah params

--- Experiment/RlkitGymMujoco/HalfCheetah/sac_train_rl.py
+++ Experiment/RlkitGymMujoco/HalfCheetah/sac_train_rl.py
@@ -4,6 +4,6 @@ from td3fd.rlkit_sac.params.default import params_config as base_params
 # td3
 params_config = deepcopy(base_params)
 params_config["config"] = ("SAC",)
-params_config["env_name"] = "Reach2DFDense"
+params_config["env_name"] = "HalfCheetah-v3"
 params_config["demo_strategy"] = "none"
 params_config["seed"] = 0
