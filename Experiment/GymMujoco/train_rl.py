1578a5d5f02fff682ef900eff7b0c7da2b6f0b99
cheneyuwu <cheneyuwu@gmail.com>
2020-01-19 14:11:19 -0500

    remove old exps

--- Experiment/GymMujoco/train_rl.py
+++
@@ -1,9 +0,0 @@
-from copy import deepcopy
-from td3fd.td3.params.td3 import params_config as base_params
-
-# TD3
-params_config = deepcopy(base_params)
-params_config["config"] = "TD3"  # experiments with the same config name will be plotted as 1 line
-params_config["env_name"] = ("InvertedPendulum-v2",)  # try also HalfCheetah-v2
-params_config["demo_strategy"] = ("none",)  # use "none" for td3, "bc" for rl+bc, "gan" for shaping
-params_config["seed"] = 1  # try also HalfCheetah-v2
