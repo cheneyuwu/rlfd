c073dc25930d82c70d4a964b22c3928d79f32af7
Yuchen Wu <cheneyuwu@gmail.com>
2020-01-21 01:35:08 -0500

    update training scripts

--- Experiment/RlkitPegInsertion2D/Noise1.0/sac_train_rlbc_qfilter.py
+++ Experiment/RlkitPegInsertion2D/Noise1.0/sac_train_rlbc_qfilter.py
@@ -2,7 +2,17 @@ from copy import deepcopy
 from td3fd.rlkit_sac.params.peginhole2d import params_config as base_params
 
 params_config = deepcopy(base_params)
-params_config["config"] = ("SAC_BC_QFilter",)
+params_config["config"] = ("SAC_BC_QFilter_1e-4",)
 params_config["demo_strategy"] = "bc"
 params_config["trainer_kwargs"]["q_filter"] = True
-params_config["seed"] = tuple(range(2))
+params_config["seed"] = tuple(range(5))
+# tune parameters
+params_config["trainer_kwargs"]["prm_loss_weight"] = (1e-4,)
+
+params_config_2 = deepcopy(base_params)
+params_config_2["config"] = ("SAC_BC_QFilter_1e-2",)
+params_config_2["demo_strategy"] = "bc"
+params_config_2["trainer_kwargs"]["q_filter"] = True
+params_config_2["seed"] = tuple(range(5))
+# tune parameters
+params_config_2["trainer_kwargs"]["prm_loss_weight"] = (1e-2,)
