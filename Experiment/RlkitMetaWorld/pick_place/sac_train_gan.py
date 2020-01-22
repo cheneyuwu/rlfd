ccb3eb436412cae3dbc7e090dca35f7edba5d845
cheneyuwu <cheneyuwu@gmail.com>
2020-01-21 21:29:22 -0500

    experiments

---
+++ Experiment/RlkitMetaWorld/pick_place/sac_train_gan.py
@@ -0,0 +1,11 @@
+from copy import deepcopy
+from td3fd.rlkit_sac.params.default import params_config as base_params
+
+params_config = deepcopy(base_params)
+params_config["config"] = ("SAC_GAN_Shaping",)
+params_config["env_name"] = "pick-place-v1"
+params_config["demo_strategy"] = "gan"
+params_config["seed"] = tuple(range(5))
+
+# tune params
+params_config["shaping"]["gan"]["latent_dim"] = 10
---
+++ Experiment/RlkitMetaWorld/pick_place/sac_train_rl.py
@@ -0,0 +1,8 @@
+from copy import deepcopy
+from td3fd.rlkit_sac.params.default import params_config as base_params
+
+params_config = deepcopy(base_params)
+params_config["config"] = ("SAC",)
+params_config["env_name"] = "pick-place-v1"
+params_config["demo_strategy"] = "none"
+params_config["seed"] = tuple(range(5))
