e556970c5bab26b8f1c7e98a39f23c0f87623b5e
Yuchen Wu <cheneyuwu@gmail.com>
2020-01-24 15:45:30 -0500

    new metaworld experiments

---
+++ Experiment/RlkitMetaWorld/drawer_close/sac_train_rlbc_qfilter.py
@@ -0,0 +1,12 @@
+from copy import deepcopy
+from td3fd.rlkit_sac.params.default import params_config as base_params
+
+params_config = deepcopy(base_params)
+params_config["config"] = ("SAC_BC_QFilter",)
+params_config["env_name"] = "drawer-close-v1"
+params_config["trainer_kwargs"]["q_filter"] = True
+params_config["demo_strategy"] = "bc"
+params_config["seed"] = tuple(range(5))
+
+# tune params
+params_config["trainer_kwargs"]["bc_criterion"] = "logprob"
---
+++ Experiment/RlkitMetaWorld/drawer_close/td3_train_rlbc.py
@@ -0,0 +1,9 @@
+from copy import deepcopy
+from td3fd.rlkit_td3.params.default import params_config as base_params
+
+params_config = deepcopy(base_params)
+params_config["config"] = ("TD3_BC",)
+params_config["env_name"] = "drawer-close-v1"
+params_config["trainer_kwargs"]["q_filter"] = False
+params_config["demo_strategy"] = "bc"
+params_config["seed"] = tuple(range(5))
