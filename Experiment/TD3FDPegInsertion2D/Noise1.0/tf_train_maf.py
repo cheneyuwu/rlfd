2fc39763441e5b986b2d6b7ac6d0bb44f6f0f5b4
cheneyuwu <cheneyuwu@gmail.com>
2020-02-03 16:32:03 -0500

    param update

---
+++ Experiment/TD3FDPegInsertion2D/Noise1.0/tf_train_maf.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+from td3fd.ddpg.params.peginhole2d import params_config as base_params
+
+# maf training
+# params_config_maf = deepcopy(base_params)
+# params_config_maf["ddpg"]["demo_strategy"] = ("nf",)
+# params_config_maf["ddpg"]["sample_demo_buffer"] = 1
+# params_config_maf["train"]["shaping_n_epochs"] = int(1e4)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["num_bijectors"] = (3,)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["layer_sizes"] = ([64, 64],)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["reg_loss_weight"] = (200.0,)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["potential_weight"] = (3.0,)
+# params_config_maf["seed"] = tuple(range(8))
+
+# maf plotting
+params_config_maf = deepcopy(base_params)
+params_config_maf["config"] = ("test_tf_MAF",)
+params_config_maf["ddpg"]["demo_strategy"] = ("nf",)
+params_config_maf["ddpg"]["sample_demo_buffer"] = True
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["num_bijectors"] = (3,)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["layer_sizes"] = ([64, 64],)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["reg_loss_weight"] = (200.0,)
+# params_config_maf["ddpg"]["shaping_params"]["nf"]["potential_weight"] = (3.0,)
+params_config_maf["seed"] = tuple(range(5))
