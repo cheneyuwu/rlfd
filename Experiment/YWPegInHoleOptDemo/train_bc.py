6b040c00924d67df8e30e3045810e03619bf74b9
cheneyuwu <cheneyuwu@gmail.com>
2020-04-03 15:46:03 -0400

    peg insertion opt demo

---
+++ Experiment/YWPegInHoleOptDemo/train_bc.py
@@ -0,0 +1,20 @@
+from copy import deepcopy
+from td3fd.ddpg2.params.peginsertionrandinit import params_config as base_params
+
+params_config = deepcopy(base_params)
+
+params_config["config"] = ("BC",)
+
+params_config["ddpg"]["demo_strategy"] = "none"
+params_config["ddpg"]["sample_demo_buffer"] = True
+
+params_config["ddpg"]["use_n_step_return"] = False
+
+# initialize the policy using behavior cloning
+params_config["ddpg"]["initialize_with_bc"] = True
+params_config["ddpg"]["initialize_num_epochs"] = 2000
+
+params_config["ddpg"]["num_batches"] = 0 # do not further train with RL
+
+# BC
+params_config["seed"] = tuple(range(4))
---
+++ Experiment/YWPegInHoleOptDemo/train_gan.py
@@ -0,0 +1,20 @@
+from copy import deepcopy
+from td3fd.ddpg2.params.peginsertionrandinit import params_config as base_params
+
+params_config = deepcopy(base_params)
+
+params_config["config"] = ("TD3_GAN_Shaping",)
+
+params_config["ddpg"]["demo_strategy"] = "gan"
+params_config["ddpg"]["sample_demo_buffer"] = True
+
+params_config["ddpg"]["use_n_step_return"] = False
+
+# initialize the policy using behavior cloning
+params_config["ddpg"]["initialize_with_bc"] = True
+params_config["ddpg"]["initialize_num_epochs"] = 2000
+
+# TD3_GAN_Shaping
+params_config["ddpg"]["shaping_params"]["gan"]["gp_lambda"] = (1.0,)
+params_config["ddpg"]["shaping_params"]["num_epochs"] = (int(1e4),)
+params_config["seed"] = tuple(range(4))
