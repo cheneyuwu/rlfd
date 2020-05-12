d6352f77e796bcc4cc4084f8150e8580b5d6ae02
cheneyuwu <cheneyuwu@gmail.com>
2020-05-12 02:26:32 -0400

    major functional change of replay buffer for mage

--- Package/rlfd/rlfd/memory.py
+++ Package/rlfd/rlfd/memory.py
@@ -1,8 +1,7 @@
 """ We implemented two types of replay buffer "UniformReplayBuffer" and "RingReplayBuffer"
     They are only different in the way they store experiences.
-    "UniformReplayBuffer" is easier to be extended to HER style methods. for future works
 """
-
+import abc
 import numpy as np
 
 
@@ -27,7 +26,7 @@ def iterbatches(arrays,
       yield tuple(a[batch_inds] for a in arrays)
 
 
-class ReplayBufferBase:
+class ReplayBuffer(object, metaclass=abc.ABCMeta):
 
   def __init__(self, buffer_shapes, size):
     """ Create a replay buffer.
@@ -35,12 +34,12 @@ class ReplayBufferBase:
         size (int) - the size of the buffer, measured in transitions
     """
     # memory management
-    self.size = size
-    self.current_size = 0
+    self._size = size
+    self._current_size = 0
 
     # buffer
     self.buffers = {
-        key: np.empty([self.size, *shape], dtype=np.float32)
+        key: np.empty([self._size, *shape], dtype=np.float32)
         for key, shape in buffer_shapes.items()
     }
 
@@ -50,51 +49,80 @@ class ReplayBufferBase:
     return episode_batch
 
   def dump_to_file(self, path):
-    if self.current_size == 0:
+    if self._current_size == 0:
       return
-    buffers = {k: v[:self.current_size] for k, v in self.buffers.items()}
+    buffers = {k: v[:self._current_size] for k, v in self.buffers.items()}
     np.savez_compressed(path, **buffers)  # save the file
 
-  def sample(self, batch_size):
+  def sample(self,
+             batch_size=None,
+             return_iterator=False,
+             shuffle=False,
+             include_partial_batch=False):
     """ Returns a dict {key: array(batch_size x shapes[key])}
         If batch_size is -1, this function should return the entire buffer (i.e. all stored transitions)
         """
-    raise NotImplementedError
+    assert self._current_size > 0, "Replay buffer is empty."
+    if return_iterator:
+      return self._sample_iterator(batch_size, shuffle, include_partial_batch)
+    else:
+      assert batch_size != None, "Must provide batch size to sample randomly."
+      return self._sample_random(batch_size)
+
+  def store(self, data):
+    """ Store data into the replay buffer.
+            data.shape[0] should be the size of increment
+        """
+    batch_sizes = [len(v) for v in data.values()]
+    assert np.all(np.array(batch_sizes) == batch_sizes[0])
+    batch_size = batch_sizes[0]
+
+    self._store(data)
+
+    # memory management
+    self._current_size = min(self._size, self._current_size + batch_size)
 
   def store_episode(self, episode_batch):
     """ API for storing episodes. Including memory management.
             episode_batch: array(batch_size x (T or T+1) x dim_key)
         """
-    batch_sizes = [len(episode_batch[key]) for key in episode_batch.keys()]
-    assert np.all(np.array(batch_sizes) == batch_sizes[0])
-    batch_size = batch_sizes[0]
-
-    idxs = self._get_storage_idx(batch_size)
-
-    # load inputs into buffers
-    for key in self.buffers.keys():
-      self.buffers[key][idxs] = episode_batch[key]
-
-    # memory management
-    self.current_size = min(self.size, self.current_size + batch_size)
+    print("Warning: store_episode method is deprecated. Use store instead.")
+    self.store(episode_batch)
 
   @property
   def full(self):
-    return self.current_size == self.size
+    return self._current_size == self._size
+
+  @property
+  def capacity(self):
+    return self._size
+
+  @property
+  def current_size(self):
+    return self._current_size
 
   def clear_buffer(self):
-    self.current_size = 0
     self._clear_buffer()
+    self._current_size = 0
 
+  @abc.abstractmethod
+  def _store(self, data):
+    """store batch of data into the replay buffer"""
+
+  @abc.abstractmethod
+  def _sample_random(self, batch_size):
+    """Sample a batch of sizee batch_size randomly from the replay buffer"""
+
+  @abc.abstractmethod
+  def _sample_iterator(self, batch_size, shuffle, include_partial_batch):
+    """return a iterator from sampler"""
+
+  @abc.abstractmethod
   def _clear_buffer(self):
     """Overwrite this for further cleanup"""
-    pass
-
-  def _get_storage_idx(self):
-    raise NotImplementedError
 
 
-class RingReplayBuffer(ReplayBufferBase):
+class RingReplayBuffer(ReplayBuffer):
 
   def __init__(self, buffer_shapes, size_in_transitions):
     """ Creates a replay buffer.
@@ -106,52 +134,75 @@ class RingReplayBuffer(ReplayBufferBase):
     super().__init__(size=size_in_transitions, buffer_shapes=buffer_shapes)
 
     # contains {key: array(transitions x dim_key)}
-    self.pointer = 0
+    self._pointer = 0
 
-  def sample(self, batch_size=-1):
-    """
-        This function returns all when batch_size is set to -1
-        """
-    idxs = np.random.randint(0, self.current_size,
-                             batch_size) if batch_size >= 0 else np.arange(
-                                 self.current_size)
-    batch_size = batch_size if batch_size >= 0 else self.current_size
-
+  def _sample_random(self, batch_size):
+    """Sample a batch of sizee batch_size randomly from the replay buffer"""
+    inds = np.random.randint(0, self._current_size, batch_size)
     transitions = {
-        key: self.buffers[key][idxs].copy() for key in self.buffers.keys()
+        key: self.buffers[key][inds].copy() for key in self.buffers.keys()
     }
-    transitions = {
-        k: transitions[k].reshape(batch_size, *transitions[k].shape[1:])
-        for k in transitions.keys()
-    }
-    assert all(
-        [transitions[k].shape[0] == batch_size for k in transitions.keys()])
+    # handle multi-step return TODO this is a hack
+    transitions["n"] = np.ones_like(transitions["r"])
 
     return transitions
 
+  def _sample_iterator(self, batch_size, shuffle, include_partial_batch):
+    """return a iterator from sampler"""
+    inds = np.arange(self._current_size)
+    transitions = {
+        key: self.buffers[key][inds].copy() for key in self.buffers.keys()
+    }
+    # handle multi-step return TODO this is a hack
+    transitions["n"] = np.ones_like(transitions["r"])
+
+    if shuffle:
+      np.random.shuffle(inds)
+    if batch_size == None:
+      batch_size = self._current_size
+    sections = np.arange(0, self._current_size, batch_size)[1:]
+
+    def _sample_iterator():
+      for batch_inds in np.array_split(inds, sections):
+        if include_partial_batch or len(batch_inds) == batch_size:
+          yield {k: v[batch_inds] for k, v in transitions.items()}
+
+    return _sample_iterator()
+
+  def _store(self, data):
+
+    batch_sizes = [len(v) for v in data.values()]
+    assert np.all(np.array(batch_sizes) == batch_sizes[0])
+    batch_size = batch_sizes[0]
+    idxs = self._get_storage_idx(batch_size)
+
+    # load inputs into buffers
+    for key in self.buffers.keys():
+      self.buffers[key][idxs] = data[key]
+
+  def _clear_buffer(self):
+    self._pointer = 0
+
   def _get_storage_idx(self, inc):
-    assert inc <= self.size, "batch committed to replay is too large!"
+    assert inc <= self._size, "batch committed to replay is too large!"
     assert inc > 0, "invalid increment"
     # go consecutively until you hit the end, and restart from the beginning.
-    if self.pointer + inc <= self.size:
-      idx = np.arange(self.pointer, self.pointer + inc)
-      self.pointer += inc
+    if self._pointer + inc <= self._size:
+      idx = np.arange(self._pointer, self._pointer + inc)
+      self._pointer += inc
     else:
-      overflow = inc - (self.size - self.pointer)
-      idx_a = np.arange(self.pointer, self.size)
+      overflow = inc - (self._size - self._pointer)
+      idx_a = np.arange(self._pointer, self._size)
       idx_b = np.arange(0, overflow)
       idx = np.concatenate([idx_a, idx_b])
-      self.pointer = overflow
+      self._pointer = overflow
 
     if inc == 1:
       idx = idx[0]
     return idx
 
-  def _clear_buffer(self):
-    self.pointer = 0
 
-
-class UniformReplayBuffer(ReplayBufferBase):
+class UniformReplayBuffer(ReplayBuffer):
 
   def __init__(self, buffer_shapes, size_in_transitions, T):
     """ Creates a replay buffer.
@@ -169,21 +220,21 @@ class UniformReplayBuffer(ReplayBufferBase):
 
   @property
   def current_size_episode(self):
-    return self.current_size
+    return self._current_size
 
   @property
   def current_size_transiton(self):
-    return self.current_size * self.T
+    return self._current_size * self.T
 
-  def sample(self, batch_size=-1):
+  def _sample_random(self, batch_size):
     """ Returns a dict {key: array(batch_size x shapes[key])}
-        """
+    """
+
     buffers = {}
-
-    assert self.current_size > 0
     for key in self.buffers.keys():
-      buffers[key] = self.buffers[key][:self.current_size]
+      buffers[key] = self.buffers[key][:self._current_size]
 
+    # TODO: remove this later
     buffers["o_2"] = buffers["o"][:, 1:, ...]
     if "ag" in buffers.keys():
       buffers["ag_2"] = buffers["ag"][:, 1:, ...]
@@ -192,12 +243,81 @@ class UniformReplayBuffer(ReplayBufferBase):
     if "pv" in buffers.keys():
       buffers["pv_2"] = buffers["pv"][:, 1:, ...]
 
-    transitions = self.sample_transitions(buffers, batch_size)
-    assert all([key in transitions for key in list(self.buffers.keys())
-               ]), "key missing from transitions"
+    episode_idxs = np.random.randint(self._current_size, size=batch_size)
+    step_idxs = np.random.randint(self.T, size=batch_size)
+
+    transitions = {
+        key: buffers[key][episode_idxs, step_idxs].copy()
+        for key in buffers.keys()
+    }
+    # handle multi-step return TODO this is a hack
+    transitions["n"] = np.ones_like(transitions["r"])
 
     return transitions
 
+  def _sample_iterator(self, batch_size, shuffle, include_partial_batch):
+
+    buffers = {}
+    for key in self.buffers.keys():
+      buffers[key] = self.buffers[key][:self._current_size]
+
+    # TODO: remove this later
+    buffers["o_2"] = buffers["o"][:, 1:, ...]
+    if "ag" in buffers.keys():
+      buffers["ag_2"] = buffers["ag"][:, 1:, ...]
+    if "g" in buffers.keys():
+      buffers["g_2"] = buffers["g"][:, :, ...]
+    if "pv" in buffers.keys():
+      buffers["pv_2"] = buffers["pv"][:, 1:, ...]
+
+    episode_idxs = np.repeat(np.arange(self._current_size), self.T)
+    step_idxs = np.tile(np.arange(self.T), self._current_size)
+
+    transitions = {
+        key: buffers[key][episode_idxs, step_idxs].copy()
+        for key in buffers.keys()
+    }
+    # handle multi-step return
+    transitions["n"] = np.ones_like(transitions["r"])
+
+    inds = np.arange(self._current_size * self.T)
+    if shuffle:
+      np.random.shuffle(inds)
+    if batch_size == None:
+      batch_size = self._current_size * self.T
+    sections = np.arange(0, self._current_size * self.T, batch_size)[1:]
+
+    def _sample_iterator():
+      for batch_inds in np.array_split(inds, sections):
+        if include_partial_batch or len(batch_inds) == batch_size:
+          yield {k: v[batch_inds] for k, v in transitions.items()}
+
+    return _sample_iterator()
+
+  # def sample(self, batch_size=-1):
+  #   """ Returns a dict {key: array(batch_size x shapes[key])}
+  #       """
+  #   buffers = {}
+
+  #   assert self._current_size > 0
+  #   for key in self.buffers.keys():
+  #     buffers[key] = self.buffers[key][:self._current_size]
+
+  #   buffers["o_2"] = buffers["o"][:, 1:, ...]
+  #   if "ag" in buffers.keys():
+  #     buffers["ag_2"] = buffers["ag"][:, 1:, ...]
+  #   if "g" in buffers.keys():
+  #     buffers["g_2"] = buffers["g"][:, :, ...]
+  #   if "pv" in buffers.keys():
+  #     buffers["pv_2"] = buffers["pv"][:, 1:, ...]
+
+  #   transitions = self.sample_transitions(buffers, batch_size)
+  #   assert all([key in transitions for key in list(self.buffers.keys())
+  #              ]), "key missing from transitions"
+
+  #   return transitions
+
+  # TODO: remove this method
   def sample_transitions(self, buffers, batch_size=-1):
     """Sample transitions of size batch_size randomly from episode_batch.
         Args:
@@ -227,19 +347,33 @@ class UniformReplayBuffer(ReplayBufferBase):
 
     return transitions
 
+  def _store(self, data):
+
+    batch_sizes = [len(v) for v in data.values()]
+    assert np.all(np.array(batch_sizes) == batch_sizes[0])
+    batch_size = batch_sizes[0]
+    idxs = self._get_storage_idx(batch_size)
+
+    # load inputs into buffers
+    for key in self.buffers.keys():
+      self.buffers[key][idxs] = data[key]
+
+  def _clear_buffer(self):
+    pass
+
   def _get_storage_idx(self, inc):
-    assert inc <= self.size, "batch committed to replay is too large!"
+    assert inc <= self._size, "batch committed to replay is too large!"
     assert inc > 0, "invalid increment"
     # go consecutively until you hit the end, and then go randomly.
-    if self.current_size + inc <= self.size:
-      idx = np.arange(self.current_size, self.current_size + inc)
-    elif self.current_size < self.size:
-      overflow = inc - (self.size - self.current_size)
-      idx_a = np.arange(self.current_size, self.size)
-      idx_b = np.random.randint(0, self.current_size, overflow)
+    if self._current_size + inc <= self._size:
+      idx = np.arange(self._current_size, self._current_size + inc)
+    elif self._current_size < self._size:
+      overflow = inc - (self._size - self._current_size)
+      idx_a = np.arange(self._current_size, self._size)
+      idx_b = np.random.randint(0, self._current_size, overflow)
       idx = np.concatenate([idx_a, idx_b])
     else:
-      idx = np.random.randint(0, self.size, inc)
+      idx = np.random.randint(0, self._size, inc)
 
     if inc == 1:
       idx = idx[0]
@@ -262,6 +396,29 @@ class MultiStepReplayBuffer(UniformReplayBuffer):
 
     self.num_steps = num_steps
     self.gamma = gamma
+
+  def sample(self, batch_size=-1):
+    """ Returns a dict {key: array(batch_size x shapes[key])}
+        """
+    buffers = {}
+
+    assert self._current_size > 0
+    for key in self.buffers.keys():
+      buffers[key] = self.buffers[key][:self._current_size]
+
+    buffers["o_2"] = buffers["o"][:, 1:, ...]
+    if "ag" in buffers.keys():
+      buffers["ag_2"] = buffers["ag"][:, 1:, ...]
+    if "g" in buffers.keys():
+      buffers["g_2"] = buffers["g"][:, :, ...]
+    if "pv" in buffers.keys():
+      buffers["pv_2"] = buffers["pv"][:, 1:, ...]
+
+    transitions = self.sample_transitions(buffers, batch_size)
+    assert all([key in transitions for key in list(self.buffers.keys())
+               ]), "key missing from transitions"
+
+    return transitions
 
   def sample_transitions(self, buffers, batch_size=-1):
     """Sample transitions of size batch_size randomly from episode_batch.
@@ -303,3 +460,103 @@ class MultiStepReplayBuffer(UniformReplayBuffer):
     assert transitions["u"].shape[0] == batch_size
 
     return transitions
+
+
+if __name__ == "__main__":
+  # UniformReplayBuffer Usage
+  o = np.linspace(0.0, 15.0, 16).reshape((2, 4, 2))  # Batch x Time x Dim
+  r = np.linspace(0.0, 5.0, 6).reshape((2, 3, 1))  # Batch x Time x Dim
+  replay_buffer = UniformReplayBuffer({
+      "o": o.shape[1:],
+      "r": r.shape[1:]
+  }, 6, 3)
+  #
+  print(replay_buffer.capacity)
+  print(replay_buffer.current_size)
+  print(replay_buffer.full)
+  #
+  replay_buffer.store({"o": o, "r": r})
+  #
+  print(replay_buffer.capacity)
+  print(replay_buffer.current_size)
+  print(replay_buffer.full)
+  #
+  batch = replay_buffer.sample(10)
+  print(batch["r"].shape)
+  print(batch["r"])
+  #
+  iterator = replay_buffer.sample(1, return_iterator=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(1, return_iterator=True, shuffle=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(4,
+                                  return_iterator=True,
+                                  shuffle=True,
+                                  include_partial_batch=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(return_iterator=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+    print("here")
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+    print("here")
+
+  # RingReplayBuffer Usage
+  o = np.linspace(0.0, 17.0, 8).reshape((4, 2))  # Batch x Time x Dim
+  r = np.linspace(0.0, 3.0, 4).reshape((4, 1))  # Batch x Time x Dim
+  replay_buffer = RingReplayBuffer({"o": o.shape[1:], "r": r.shape[1:]}, 6)
+  #
+  print(replay_buffer.capacity)
+  print(replay_buffer.current_size)
+  print(replay_buffer.full)
+  #
+  replay_buffer.store({"o": o, "r": r})
+  replay_buffer.store({"o": o, "r": r})
+  #
+  print(replay_buffer.capacity)
+  print(replay_buffer.current_size)
+  print(replay_buffer.full)
+  #
+  batch = replay_buffer.sample(10)
+  print(batch["r"].shape)
+  print(batch["r"])
+  #
+  iterator = replay_buffer.sample(1, return_iterator=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(1, return_iterator=True, shuffle=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(4,
+                                  return_iterator=True,
+                                  shuffle=True,
+                                  include_partial_batch=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+  #
+  iterator = replay_buffer.sample(return_iterator=True)
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+    print("here")
+  for batch in iterator:
+    print(batch["r"].shape)
+    print(batch["r"])
+    print("here")
