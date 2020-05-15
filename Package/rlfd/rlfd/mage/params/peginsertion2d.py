2110e26a464a3ab0599eceaa0310a13b3fd0f35d
cheneyuwu <cheneyuwu@gmail.com>
2020-05-15 16:38:43 -0400

    peg insertion 2d parameter

--- Package/rlfd/rlfd/mage/params/peginsertion2d.py
+++ Package/rlfd/rlfd/mage/params/peginsertion2d.py
@@ -12,6 +12,7 @@ params_config["ddpg"]["num_batches"] = 40
 params_config["ddpg"]["batch_size"] = 256
 params_config["ddpg"]["layer_sizes"] = [256, 256]
 params_config["ddpg"]["polyak"] = 0.95
+params_config["ddpg"]["model_update_interval"] = 400
 params_config["ddpg"]["shaping_params"]["num_epochs"] = int(1e4)
 params_config["ddpg"]["shaping_params"]["nf"]["reg_loss_weight"] = 4e2
 params_config["ddpg"]["shaping_params"]["nf"]["potential_weight"] = 1e1
