eae2ebca67e37d66c796f25b1245d1fa6ebad4a7
cheneyuwu <cheneyuwu@gmail.com>
2020-05-15 03:23:04 -0400

    update parameters

--- Package/rlfd/rlfd/mage/params/gym_mujoco.py
+++ Package/rlfd/rlfd/mage/params/gym_mujoco.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
 from ..config import default_params
 
-params_config = default_params
+params_config = deepcopy(default_params)
+params_config["ddpg"]["random_exploration_cycles"] = int(5e3)
