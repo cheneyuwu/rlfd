cf9921223feec5f177adbb07dcf260e3b9d1cc61
cheneyuwu <cheneyuwu@gmail.com>
2020-07-15 11:54:30 -0400

    update sac default params

--- Experiment/D4RL/halfcheetah-expert/sac.py
+++ Experiment/D4RL/halfcheetah-expert/sac.py
@@ -13,7 +13,6 @@ params_config["agent"]["online_data_strategy"] = "None"
 # Tuned values
 params_config["seed"] = tuple(range(5))
 
-params_config["agent"]["norm_obs"] = (True, False)
 params_config["agent"]["q_lr"] = (1e-4, 3e-4)
 params_config["agent"]["pi_lr"] = (1e-4, 3e-4)
 params_config["seed"] = tuple(range(4))
