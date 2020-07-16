9b1c5a415d26a70043ab15f5ca924441dc000696
Yuchen Wu <cheneyuwu@gmail.com>
2020-07-16 14:25:48 -0400

    experiment params

--- Experiment/D4RL/halfcheetah-expert/cql_dp.py
+++ Experiment/D4RL/halfcheetah-expert/cql_dp.py
@@ -21,10 +21,19 @@ params_config["seed"] = tuple(range(5))
 # params_config["agent"]["cql_tau"] = (0.0, 10.0)
 # params_config["seed"] = tuple(range(4))
 
-# Exp 2 Normal CQL but with online fine-tuning
-params_config["config"] = ("CQLDP_NoMaxTerm_w_Online",)
+# # Exp 2 CQL with online fine-tuning using CQL
+# # Note: This is a change in code not reflected in the parameters.
+# params_config["config"] = ("CQLDP_Offline_NoMax_CQLDP_Online",)
+# params_config["agent"]["norm_obs_offline"] = (True,)
+# params_config["num_epochs"] = (int(1e3),)
+# params_config["agent"]["pi_lr"] = (3e-5,)
+# params_config["agent"]["cql_tau"] = (0.0,)
+# params_config["seed"] = tuple(range(4))
+
+# Exp 3 CQL with online fine-tuning using SAC
+params_config["config"] = ("CQLDP_Offline_NoMax_SAC_Online",)
 params_config["agent"]["norm_obs_offline"] = (True,)
 params_config["num_epochs"] = (int(1e3),)
 params_config["agent"]["pi_lr"] = (3e-5,)
-params_config["agent"]["cql_tau"] = (10.0,)
+params_config["agent"]["cql_tau"] = (0.0,)
 params_config["seed"] = tuple(range(4))
