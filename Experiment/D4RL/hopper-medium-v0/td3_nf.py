c0c3197f57e8bc49da70a04ec688f7728c1831e8
cheneyuwu <cheneyuwu@gmail.com>
2020-07-21 17:54:29 -0400

    hopper medium exps

---
+++ Experiment/D4RL/hopper-medium-v0/td3_nf.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+from rlfd.params.td3 import gym_mujoco_params
+from rlfd.params.shaping import gym_mujoco_nf_params
+
+params_config = deepcopy(gym_mujoco_params)
+shaping_params_config = deepcopy(gym_mujoco_nf_params)
+
+params_config["config"] = ("TD3_NFShaping",)
+
+params_config["env_name"] = "hopper-medium-v0"
+# ORL Shaping
+params_config["shaping"] = shaping_params_config
+# No offline training
+params_config["offline_num_epochs"] = 0
+# No online training with offline data
+params_config["agent"]["online_data_strategy"] = "Shaping"
+# Tuned values
+params_config["seed"] = tuple(range(5))
+
+# Exp 1
+params_config["seed"] = tuple(range(4))
