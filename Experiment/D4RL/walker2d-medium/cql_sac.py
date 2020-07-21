e6b0c445b76354780e0f874df2e9c5d12a0eb371
cheneyuwu <cheneyuwu@gmail.com>
2020-07-21 13:37:12 -0400

    experiments for walker2d medium

---
+++ Experiment/D4RL/walker2d-medium/cql_sac.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+from rlfd.params.cql import gym_mujoco_params
+
+params_config = deepcopy(gym_mujoco_params)
+
+params_config["config"] = ("CQL",)
+
+params_config["env_name"] = "walker2d-medium-v0"
+# Offline training
+params_config["offline_num_epochs"] = int(1e3)
+# No online training with offline data
+params_config["agent"]["online_data_strategy"] = "None"
+# Tuned values
+params_config["seed"] = tuple(range(5))
+
+# Exp 1 CQL offline with SAC online
+params_config["config"] = ("CQL_Offline_SAC_Online",)
+params_config["agent"]["norm_obs_offline"] = (True,)
+params_config["num_epochs"] = (int(1e3),)
+params_config["agent"]["pi_lr"] = (3e-5,)
+params_config["agent"]["cql_tau"] = (0.0,)
+params_config["agent"]["auto_cql_alpha"] = (False,)
+params_config["agent"]["cql_log_alpha"] = (2.0,)
+params_config["seed"] = tuple(range(4))
---
+++ Experiment/D4RL/walker2d-medium/sac_gan.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+from rlfd.params.sac import gym_mujoco_params
+from rlfd.params.shaping import gym_mujoco_gan_params
+
+params_config = deepcopy(gym_mujoco_params)
+shaping_params_config = deepcopy(gym_mujoco_gan_params)
+
+params_config["config"] = ("SAC_GANShaping",)
+
+params_config["env_name"] = "walker2d-medium-v0"
+# ORL Shaping
+params_config["shaping"] = shaping_params_config
+# No offline training
+params_config["offline_num_epochs"] = 0
+# No online training with offline data
+params_config["agent"]["online_data_strategy"] = "Shaping"
+# Tuned values
+params_config["seed"] = tuple(range(5))
+
+# Exp 1
+params_config["seed"] = tuple(range(4))
