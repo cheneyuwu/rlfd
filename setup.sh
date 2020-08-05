9f2ba4f11404d6e911e87f0d48210ed32c423f10
Yuchen Wu <cheneyuwu@gmail.com>
2020-08-05 07:35:20 -0400

    remove unused env var

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Setup environment variables for logging.
 export RLFD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export TEMPDIR=${RLFD}/temp
-export LOGDIR=${TEMPDIR}/log
 # TensorFlow: use deterministic operations and disable warnings
 export TF_DETERMINISTIC_OPS=1
 export TF_CPP_MIN_LOG_LEVEL=3
